ad900f7f7dd65867bf2c02a8e7221612ee103a8f
Gautham <158817524+Gautham-25@users.noreply.github.com>
2024-06-28 22:55:18 +0530

    TODO.js

--- todoapp/client/src/Views/TODO/TODO.js
+++ todoapp/client/src/Views/TODO/TODO.js
@@ -1,22 +1,26 @@
-import { useEffect, useState } from 'react'
-import Styles from './TODO.module.css'
-import { dummy } from './dummy'
+import { useEffect, useState } from 'react';
+import Styles from './TODO.module.css';
+import { dummy } from './dummy';
 import axios from 'axios';
 
 export function TODO(props) {
 
-    const [newTodo, setNewTodo] = useState('')
+    const [newTodo, setNewTodo] = useState('');
+    const [newDescription, setNewDescription] = useState('');
     const [todoData, setTodoData] = useState(dummy);
     const [loading, setLoading] = useState(true);
+    const [editTodo, setEditTodo] = useState(null);
+    const [editTitle, setEditTitle] = useState('');
+    const [editDescription, setEditDescription] = useState('');
 
     useEffect(() => {
         const fetchTodo = async () => {
-            const apiData = await getTodo()
+            const apiData = await getTodo();
             setTodoData(apiData);
-            setLoading(false)
-        }
+            setLoading(false);
+        };
         fetchTodo();
-    }, [])
+    }, []);
 
     const getTodo = async () => {
         const options = {
@@ -25,15 +29,15 @@ export function TODO(props) {
             headers: {
                 accept: "application/json",
             }
-        }
+        };
         try {
-            const response = await axios.request(options)
-            return response.data
+            const response = await axios.request(options);
+            return response.data;
         } catch (err) {
             console.log(err);
             return []; // return an empty array in case of error
         }
-    }
+    };
 
     const addTodo = () => {
         const options = {
@@ -43,19 +47,22 @@ export function TODO(props) {
                 accept: "application/json",
             },
             data: {
-                title: newTodo
+                title: newTodo,
+                description: newDescription
             }
-        }
+        };
         axios
             .request(options)
             .then(function (response) {
-                console.log(response.data)
-                setTodoData(prevData => [...prevData, response.data.newTodo])
+                console.log(response.data);
+                setTodoData(prevData => [...prevData, response.data.newTodo]);
+                setNewTodo('');
+                setNewDescription('');
             })
             .catch((err) => {
-                console.log(err)
-            })
-    }
+                console.log(err);
+            });
+    };
 
     const deleteTodo = (id) => {
         const options = {
@@ -64,20 +71,20 @@ export function TODO(props) {
             headers: {
                 accept: "application/json",
             }
-        }
+        };
         axios
             .request(options)
             .then(function (response) {
-                console.log(response.data)
-                setTodoData(prevData => prevData.filter(todo => todo._id !== id))
+                console.log(response.data);
+                setTodoData(prevData => prevData.filter(todo => todo._id !== id));
             })
             .catch((err) => {
-                console.log(err)
-            })
+                console.log(err);
+            });
     };
 
-    const updateTodo = (id) => {
-        const todoToUpdate = todoData.find(todo => todo._id === id)
+    const updateTodo = (id, title, description) => {
+        const todoToUpdate = todoData.find(todo => todo._id === id);
         const options = {
             method: "PATCH",
             url: `http://localhost:8000/api/todo/${id}`,
@@ -86,26 +93,45 @@ export function TODO(props) {
             },
             data: {
                 ...todoToUpdate,
-                done: !todoToUpdate.done
+                title: title || todoToUpdate.title,
+                description: description || todoToUpdate.description,
+                done: todoToUpdate.done
             }
-        }
+        };
         axios
             .request(options)
             .then(function (response) {
-                console.log(response.data)
-                setTodoData(prevData => prevData.map(todo => todo._id === id ? response.data : todo))
+                console.log(response.data);
+                setTodoData(prevData => prevData.map(todo => todo._id === id ? response.data : todo));
             })
             .catch((err) => {
-                console.log(err)
-            })
+                console.log(err);
+            });
+    };
+
+    const startEditing = (todo) => {
+        setEditTodo(todo._id);
+        setEditTitle(todo.title);
+        setEditDescription(todo.description);
+    };
+
+    const cancelEditing = () => {
+        setEditTodo(null);
+        setEditTitle('');
+        setEditDescription('');
+    };
+
+    const saveEdit = (id) => {
+        updateTodo(id, editTitle, editDescription);
+        setEditTodo(null);
+        setEditTitle('');
+        setEditDescription('');
     };
 
     return (
         <div className={Styles.ancestorContainer}>
             <div className={Styles.headerContainer}>
-                <h1>
-                    Tasks
-                </h1>
+                <h1>Tasks</h1>
                 <span>
                     <input
                         className={Styles.todoInput}
@@ -113,17 +139,25 @@ export function TODO(props) {
                         name='New Todo'
                         value={newTodo}
                         onChange={(event) => {
-                            setNewTodo(event.target.value)
+                            setNewTodo(event.target.value);
                         }}
+                        placeholder="Title"
+                    />
+                    <input
+                        className={Styles.todoInput}
+                        type='text'
+                        name='New Description'
+                        value={newDescription}
+                        onChange={(event) => {
+                            setNewDescription(event.target.value);
+                        }}
+                        placeholder="Description"
                     />
                     <button
                         id='addButton'
                         name='add'
                         className={Styles.addButton}
-                        onClick={() => {
-                            addTodo()
-                            setNewTodo('')
-                        }}
+                        onClick={addTodo}
                     >
                         + New Todo
                     </button>
@@ -144,16 +178,40 @@ export function TODO(props) {
                                             updateTodo(entry._id);
                                         }}
                                     />
-                                    {entry.title}
+                                    
+                                    {editTodo === entry._id ? (
+                                        <>
+                                            <input
+                                                type="text"
+                                                value={editTitle}
+                                                onChange={(e) => setEditTitle(e.target.value)}
+                                            />
+                                            <input className={Styles.a}
+                                                type="text"
+                                                value={editDescription}
+                                                onChange={(e) => setEditDescription(e.target.value)}
+                                            />
+                                        </>
+                                    ) : (
+                                        <>
+                                            <strong className={Styles.todoTitle}>{entry.title}</strong>
+                                            <p className={Styles.todoDescription}>{entry.description}</p>
+                                        </>
+                                    )}
                                 </span>
-                                <span
-                                    style={{ cursor: 'pointer' }}
-                                    onClick={() => {
-                                        deleteTodo(entry._id);
-                                    }}
-                                >
+                                <span style={{ cursor: 'pointer' }} onClick={() => deleteTodo(entry._id)}>
                                     Delete
                                 </span>
+                                {editTodo === entry._id ? (
+                                    <span>
+                                        <button onClick={() => saveEdit(entry._id)}>Save</button>
+                                        <button onClick={cancelEditing}>Cancel</button>
+                                    </span>
+                                ) : (
+                                    <span style={{ cursor: 'pointer' }} onClick={() => startEditing(entry)}>
+                                        Edit
+                                    </span>
+                                )}
                             </div>
                         ))
                     ) : (
@@ -162,5 +220,5 @@ export function TODO(props) {
                 )}
             </div>
         </div>
-    )
+    );
 }
